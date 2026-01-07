62f0a9276ff7882af1b3f0641168614f5a67b1df
hemesh-chintala <hemeshchintala3@gmail.com>
2026-01-07 12:46:35 +0530

    Two Sum

--- leetcode_1.java
+++ leetcode_1.java
@@ -1,3 +1,7 @@
+//01
+//Two Sum
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //int arr[]=new int[2];
